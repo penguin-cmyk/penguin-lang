6f879273a6fe9cd6786b6185b8c94c489d65adc4
penguin <slomogurl37@gmail.com>
2025-01-27 16:51:50 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,15 @@
 mod util;
 use std::fs;
-use util::{Tokenizer,Parser,Interpreter};
-
-/*
-   Todo:
-       - Add variables ( numbers, arrays, maybe tuples, strings )
-       - Add if | else | else if statements
-       - Also add keywords
-       - Add operators
-       - Add loops
-       - Add standard library ( File system, ...)
-       - Add types (f32, char,...)
-       - Add return
-       - Add comments
-       - (later) -> add back support for rust crates
- */
+use util::Lexer;
 
 fn main() {
-    let code = match fs::read_to_string("main.penguin") {
-        Ok(content) => content,
-        _ => {eprintln!("main.penguin does not exist");return ();}
+    let code: String = match fs::read_to_string("src/main.p") {
+        Ok(cod) => cod,
+        _ => panic!("File doesnt exist"),
     };
-
-    let tokens = Tokenizer::new(&code.as_str()).tokenize();
-    let ast = Parser::new(tokens).parse().unwrap();
-
-    Interpreter::run(ast);
+    let tokens = Lexer::new(&code.as_str()).lex();
+    
+    for token in &tokens {
+        println!("{:?}",token);
+    }
 }
